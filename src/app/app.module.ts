5824afb57f444d1fcccfbd681dd53259f7e8a2c6
Ali Ahmad <aliahmadcse@gmail.com>
2021-08-29 23:48:25 +0500

    logified the contact page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListingsPageComponent } from './listings-page/listings-page.component';
-import { ListingDetailPageComponent } from './listing-detail-page/listing-detail-page.component';
-import { ContactPageComponent } from './contact-page/contact-page.component';
-import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
-import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
-import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {AppComponent} from "./app.component";
+import {ListingsPageComponent} from "./listings-page/listings-page.component";
+import {ListingDetailPageComponent} from "./listing-detail-page/listing-detail-page.component";
+import {ContactPageComponent} from "./contact-page/contact-page.component";
+import {MyListingsPageComponent} from "./my-listings-page/my-listings-page.component";
+import {NewListingsPageComponent} from "./new-listings-page/new-listings-page.component";
+import {EditListingPageComponent} from "./edit-listing-page/edit-listing-page.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -18,13 +19,11 @@ import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.
     ContactPageComponent,
     MyListingsPageComponent,
     NewListingsPageComponent,
-    EditListingPageComponent
+    EditListingPageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {fakeListings} from "@app/fake-data";
+import {Listing} from "@app/types";
 
 @Component({
-  selector: 'app-contact-page',
-  templateUrl: './contact-page.component.html',
-  styleUrls: ['./contact-page.component.css']
+  selector: "app-contact-page",
+  templateUrl: "./contact-page.component.html",
+  styleUrls: ["./contact-page.component.css"],
 })
 export class ContactPageComponent implements OnInit {
+  private _email: string = "";
+  private _message: string = "";
+  private _listing: Listing | null = null;
+  private _id: string | null = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private route: ActivatedRoute, private router: Router) {
   }
 
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get("id");
+    this.listing =
+      fakeListings.find((listing) => listing.id === this.id) ?? null;
+    this.message = `I am interested in your ${this.listing?.name.toLowerCase()}!`;
+  }
+
+  public sendMessage(): void {
+    alert("Your Message has been sent");
+    this.router.navigateByUrl("/listings");
+  }
+
+  public get listing() {
+    return this._listing;
+  }
+
+  public set listing(listing) {
+    this._listing = listing;
+  }
+
+  public get message() {
+    return this._message;
+  }
+
+  public set message(message) {
+    this._message = message;
+  }
+
+  public get email() {
+    return this._email;
+  }
+
+  public set email(email) {
+    this._email = email;
+  }
+
+  public get id() {
+    return this._id;
+  }
+
+  public set id(id) {
+    this._id = id;
+  }
 }
