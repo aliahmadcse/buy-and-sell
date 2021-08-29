bd740c138c1cb71387da32e714ad97e4c1ae0856
Ali Ahmad <aliahmadcse@gmail.com>
2021-08-29 22:07:48 +0500

    📦 NEW: implements listings and listing detail page

--- src/app/listing-detail-page/listing-detail-page.component.ts
+++ src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { fakeListings } from "@app/fake-data";
+import { Listing } from "@app/types";
 
 @Component({
-  selector: 'app-listing-detail-page',
-  templateUrl: './listing-detail-page.component.html',
-  styleUrls: ['./listing-detail-page.component.css']
+  selector: "app-listing-detail-page",
+  templateUrl: "./listing-detail-page.component.html",
+  styleUrls: ["./listing-detail-page.component.css"],
 })
 export class ListingDetailPageComponent implements OnInit {
+  private _listing: Listing | null = null;
+  private _id: string | null = null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this._id = this.route.snapshot.paramMap.get("id");
+    this._listing =
+      fakeListings.find((listing) => listing.id === this.id) ?? null;
   }
 
+  public get listing() {
+    return this._listing;
+  }
+
+  public get id() {
+    return this._id;
+  }
 }
--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { fakeListings } from "@app/fake-data";
+import { Listing } from "@app/types";
 
 @Component({
-  selector: 'app-listings-page',
-  templateUrl: './listings-page.component.html',
-  styleUrls: ['./listings-page.component.css']
+  selector: "app-listings-page",
+  templateUrl: "./listings-page.component.html",
+  styleUrls: ["./listings-page.component.css"],
 })
 export class ListingsPageComponent implements OnInit {
+  private _listings: Listing[] = [];
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this._listings = fakeListings;
   }
 
+  public get listings() {
+    return this._listings;
+  }
 }
