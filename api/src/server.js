83df832653bf696ab91c5ca3c1b7cb67121c508a
Ali Ahmad <aliahmadcse@gmail.com>
2022-01-07 01:44:53 +0500

    database connection

--- api/src/server.js
+++ api/src/server.js
@@ -1,21 +1,40 @@
 import Hapi from "@hapi/hapi";
 import routes from "./routes";
+import { db } from "./database";
+
+let server = undefined;
 
 const start = async () => {
-  const server = Hapi.server({
+  server = Hapi.server({
     port: 8000,
     host: "localhost",
   });
 
   routes.forEach((route) => server.route(route));
 
+  db.connect();
   await server.start();
   console.log(`Server is listening on ${server.info.uri}`);
 };
 
+/**
+ * This event triggers in case of some unknown exception
+ */
 process.on("unhandledRejection", (err) => {
   console.log(err);
-  process.exit();
+  process.exit(1);
+});
+
+/**
+ * This event trigger, when user tries to kill server forcefully, through ctrl+c
+ */
+process.on("SIGINT", async () => {
+  console.log("Stopping Server");
+
+  await server.stop({ timeout: 10000 });
+  db.end();
+  console.log("Server Stopped");
+  process.exit(0);
 });
 
 start();
