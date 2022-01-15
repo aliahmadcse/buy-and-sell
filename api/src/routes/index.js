897a318e6c25eeeb0471d79ff70303b9c1bd1ad3
Ali Ahmad <aliahmadcse@gmail.com>
2022-01-15 23:54:10 +0500

    updates listing route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,10 +2,14 @@ import { getAllListingsRoute } from "./getAllListing";
 import { getListingRoute } from "./getListing";
 import { addViewToListingRoute } from "./addViewToListing";
 import { getUserListingRoute } from "./getUserListing";
+import { createNewListingRoute } from "./createNewListing";
+import { updateListingRoute } from "./updateListing";
 
 export default [
   getAllListingsRoute,
   getListingRoute,
   addViewToListingRoute,
   getUserListingRoute,
+  createNewListingRoute,
+  updateListingRoute,
 ];
